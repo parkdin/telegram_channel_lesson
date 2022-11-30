e889eb5c3872d663e54e05bee2c7df3a33102760
ikromyusupov <@ikromyusupovdev@gmail.com>
2022-12-01 00:28:47 +0500

    added new tasks

---
+++ get_number_of_posts.py
@@ -0,0 +1,12 @@
+from read_data import fromJson
+
+def get_number_of_posts(data:dict)->int:
+    """
+    Return the number of posts for a given dictionary
+
+    args:
+        data: a dictionary of posts
+
+    returns: the number of posts for the given dictionary
+    """
+    return 0
---
+++ get_posts_peer_day.py
@@ -0,0 +1,9 @@
+from read_data import fromJson
+def get_posts_peer_day(data:dict)->dict:
+    """
+    Return the number of posts for each month
+
+    args:
+        data: a dictionary of posts
+    returns: a dictionary with the number of posts for each month
+    """
